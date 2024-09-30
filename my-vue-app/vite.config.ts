fe0bd5357b6d06f228036e6124e1600c538aef2a
apple <khondokarmehraz2002@gmail.com>
2024-09-30 19:30:49 +0600

    Prepare files for LFS

--- my-vue-app/vite.config.ts
+++ my-vue-app/vite.config.ts
@@ -1,7 +1,3 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:fed67fc2f23af27f719ea8b83e51b5e3c825c5ed73c105fb1f63b59f6afd6100
+size 157
